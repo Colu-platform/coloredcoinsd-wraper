167bb06db15dda97a855f160fcc35c352ace639e
Oded Leiba <oded@colu.co>
2016-11-09 11:18:08 +0200

    test fix

--- test/test.js
+++ test/test.js
@@ -16,22 +16,13 @@ describe('Test coloredcoinsd', function () {
   var unsigntx = '0100000001d542409c9006cf73af5a09fa6814156b635b57a7c8929c7ad6d514254bb6d108020000001976a9143dccfff7d33c163957d94949789baf660bed5a6c88acffffffff0358020000000000001976a9140964477fbc5bcce8c2ddbd8b4c705ef60c5a91e788ac00000000000000000a6a084343010501000110207a0100000000001976a9143dccfff7d33c163957d94949789baf660bed5a6c88ac00000000'
   var expectedsigntx = '0100000001d542409c9006cf73af5a09fa6814156b635b57a7c8929c7ad6d514254bb6d108020000006a47304402207b67c24b1602aef5e9da57685a1bf19ad4267f331ef061cfeace70ba7ab119b302206928a96dc9a86c443190759fddf2e372aed450305954a2db9deb36dc2a6115fd01210240042f2cfb410b4fab76a33dd36376fc752b03ee6f14708da6cd4d306670068bffffffff0358020000000000001976a9140964477fbc5bcce8c2ddbd8b4c705ef60c5a91e788ac00000000000000000a6a084343010501000110207a0100000000001976a9143dccfff7d33c163957d94949789baf660bed5a6c88ac00000000'
   var privateKey = bitcoin.ECKey.fromWIF('cQ176k8LDck5aNJTQcXd7G4rCqGM3jhJyZ7MNawyzAfaWuVpP5Xb')
-  var issuerAssetId = 'U9a36XP1UwL5pxaYYiZYJ86sUqWAJ2dGbLaer'
+  var issuerAssetId = 'La39iBSrbqSvsvtJj7SPJSsCqw6njDewER7dQk'
   var issuerJson = {
-      "social":{
-          "twitter":{            
-              "username":"assaf_colu"
-          },
-          "facebook":{
-              "page_id": 791150224328487
-          },
-          "github":{
-              "gist_id":"1c214aa9d1a74133bec3"        
-          }
-      },
-      "domain":{
-          "url":"https://www.bankofamerica.com/assets.txt"
+    "social": {
+      "facebook": {
+        "page_id": 1790094654575050
       }
+    }
   }
 
   it('Should create issue tx.', function (done) {
@@ -165,7 +156,7 @@ describe('Test coloredcoinsd', function () {
     cc.verifyIssuer(issuerAssetId, issuerJson, function (err, ans) {
       assert.ifError(err)
       expect(ans).to.have.property('verifications')
-      assert(ans.verifications)
+      assert(ans.verifications.social.facebook)
       done()
     })
   })
